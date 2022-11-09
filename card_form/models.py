d552efab2b2a47fcf44bf81a25a8c6fd5ffac9dd
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-09 11:20:17 +0000

    created a CustomerCard model for Form data

--- card_form/models.py
+++ card_form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CustomerCard(models.Model):
+    cardholder_name = models.CharField(max_length=100)
+    card_number = models.CharField(max_length=16)
+    expiry_month = models.IntegerField()
+    expiry_year = models.IntegerField()
+    cvc = models.IntegerField()
+
+    def __str__(self):
+        name_arr = self.cardholder_name.split('')
+        formatted_name = ' '.join([name_arr[0][0], name_arr[1]])
+        return formatted_name
