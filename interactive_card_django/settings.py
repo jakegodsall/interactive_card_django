cf35bd9d8b3cf7c13591017e0678bf32a05062d5
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-13 16:46:32 +0000

    added environment variable for DEBUG

--- interactive_card_django/settings.py
+++ interactive_card_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('IS_PRODUCTION')
 
 ALLOWED_HOSTS = [
     getenv('APP_HOST')
