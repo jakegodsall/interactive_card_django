ce81d6aca69c2680ea3a9d58d657ad9b39e587b0
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-15 11:55:28 +0000

    added path objects to class-based views

--- card_form/urls.py
+++ card_form/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import CardFormView, thank_you
+from .views import CardFormView, ThankYouView
 
 urlpatterns = [
     path('', CardFormView.as_view(), name="card_form"),
-    path('thank-you', thank_you, name="thank_you")
+    path('thank-you', ThankYouView.as_view(), name="thank_you")
 ]
