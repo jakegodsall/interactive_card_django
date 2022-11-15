ce86d7dc3fc53eae96bbc7748113cab534add276
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-15 11:55:51 +0000

    removed unnecesary __str__() on model

--- card_form/models.py
+++ card_form/models.py
@@ -4,15 +4,12 @@ from django.db import models
 
 
 class CustomerCard(models.Model):
+    id = models.AutoField(primary_key=True)
     cardholder_name = models.CharField(max_length=100)
     card_number = models.CharField(max_length=16)
     expiry_month = models.IntegerField()
     expiry_year = models.IntegerField()
     cvc = models.IntegerField()
-
-    # def __str__(self):
-    #     split_name = self.cardholder_name.split(' ')
-    #     return ' '.join([f"{split_name[0][0]}.", split_name[1]])
 
     def __str__(self):
         return self.cardholder_name
