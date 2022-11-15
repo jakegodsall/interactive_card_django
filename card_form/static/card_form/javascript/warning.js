1306789fd6305e56d3f393dc528990f62308f4fd
jakegodsall <jake.edward.godsall@gmail.com>
2022-11-15 10:58:59 +0000

    added session storage to remember warning acceptance

--- card_form/static/card_form/javascript/warning.js
+++ card_form/static/card_form/javascript/warning.js
@@ -1,12 +1,15 @@
-const warningTemplate = document.getElementById('warning-template');
-const backgroundImage = document.getElementById('background-image');
+if (sessionStorage.getItem('warning-accepted') === null) {
+    const warningTemplate = document.getElementById('warning-template');
+    const backgroundImage = document.getElementById('background-image');
 
-const clone = warningTemplate.content.cloneNode(true);
+    const clone = warningTemplate.content.cloneNode(true);
 
-document.body.insertBefore(clone, backgroundImage);
+    document.body.insertBefore(clone, backgroundImage);
 
-const warningAccept = document.getElementById('warning-accept');
+    const warningAccept = document.getElementById('warning-accept');
 
-warningAccept.addEventListener('click', () => {
-    document.getElementById('warning-overlay').remove();
-});
+    warningAccept.addEventListener('click', () => {
+        document.getElementById('warning-overlay').remove();
+        sessionStorage.setItem('warning-accepted', true);
+    });
+}
